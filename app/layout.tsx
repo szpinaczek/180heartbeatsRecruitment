9acfdbd6f91dec98c82285ab3b055794fda093ec
szpinaczek <szpinaczek11@gmail.com>
2024-12-22 18:51:05 +0100

    add header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
         <main className="flex min-h-screen flex-col items-center justify-center p-10 lg:pt-24 lg:pl-24 lg:pr-24 lg:pb-0 bg-white">
           {children}
         </main>
---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function header() {
+    return <header className="header flex flex-row justify-between items-center gap-10 px-20 py-10">
+        <Link href="/">Home</Link> 
+        <Link href="/module1">Module 1</Link> 
+        <Link href="/module2">Module 2</Link> 
+        <Link href="/module4">Module 4</Link> 
+    </header>
+}
